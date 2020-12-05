ea227667e17601ea8e00476a5159dbca40939e69
jonyataco <jon.yataco@gmail.com>
2020-12-04 22:28:20 -0500

    Implemented tab bar and two view controllers. Added asset files as well.

---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,28 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Jonathan Yataco  on 12/3/20.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
