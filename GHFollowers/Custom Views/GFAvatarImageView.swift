6b062581cd531690c6a7e14ccec27f3929f96525
jonyataco <jon.yataco@gmail.com>
2020-12-20 22:58:53 -0500

    Created FollowerCell

---
+++ GHFollowers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,32 @@
+//
+//  GFAvatarImageView.swift
+//  GHFollowers
+//
+//  Created by Jonathan Yataco  on 12/20/20.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+
+    let placeholderImage = UIImage(named: "avatar-placeholder")!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure() {
+        layer.cornerRadius = 10
+        // Have to do this otherwise the underlying UIImage would still show
+        // with sharp corners even though the UIImageView has rounded corner radius
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
