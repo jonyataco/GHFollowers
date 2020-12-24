37339fb3c893238c9f58f473ad0e9960422151f2
jonyataco <jon.yataco@gmail.com>
2020-12-24 15:06:56 -0500

    LoadingView implemented
    When waiting for the network call to finish on FollowerListVC, now the
    view controller will show a loading indicator that is overlayed the VC.
    Created by extending UIViewController so that it can be used throughout
    the app.

--- GHFollowers/Extensions/UIViewController+Ext.swift
+++ GHFollowers/Extensions/UIViewController+Ext.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+fileprivate var containerView: UIView!
+
 extension UIViewController {
     
     func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
@@ -15,6 +17,40 @@ extension UIViewController {
             alertVC.modalPresentationStyle = .overFullScreen
             alertVC.modalTransitionStyle = .crossDissolve
             self.present(alertVC, animated: true)
+        }
+    }
+    
+    func showLoadingView() {
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+        containerView.backgroundColor = .systemBackground
+        // Starting alpha at zero because we want to do a small animation up to .8
+        containerView.alpha = 0
+        
+        UIView.animate(withDuration: 0.25) {
+            containerView.alpha = 0.8
+        }
+        
+        // Creating an activity indicator view and adding it to the loading view
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        containerView.addSubview(activityIndicator)
+        
+        // Putting the activity indicator in the center of the container view
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            activityIndicator.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
+        ])
+        
+        // Star the animation
+        activityIndicator.startAnimating()
+        
+    }
+    
+    func dismissLoadingView() {
+        DispatchQueue.main.async {
+            containerView.removeFromSuperview()
+            containerView = nil
         }
     }
 }
