d212ca534fcf0004a1836807b1835f77c0335d13
jonyataco <jon.yataco@gmail.com>
2020-12-25 10:49:30 -0500

    Added getUserInfo network call to network manager

--- GHFollowers/Screens/UserInfoVC.swift
+++ GHFollowers/Screens/UserInfoVC.swift
@@ -15,6 +15,16 @@ class UserInfoVC: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         configureNavigationBar()
+        NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let user):
+                print(user)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Error", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
     }
     
     @objc func dismissVC() {
